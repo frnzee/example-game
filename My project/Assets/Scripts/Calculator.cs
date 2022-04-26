d3944e6f3f2f9e1d254f7c26700a1ec80e7f0b25
frnzee <b.frnzee@gmail.com>
2022-04-27 00:31:36 +0300

    Переделана механика, добавлены цифровые кнопки и дополнительные функции

--- My project/Assets/Scripts/Calculator.cs
+++ My project/Assets/Scripts/Calculator.cs
@@ -6,104 +6,142 @@ using UnityEngine.EventSystems;
 public class Calculator : MonoBehaviour
 {
     public Text Result;
-    public InputField textField1;
-    public InputField textField2;
+    public Text CurrentTask;
     private float _value1;
     private float _value2;
+    private int operationSelector;
+    public void AddNumeric(int number)
+    {
+        Result.text += number;
+    }
+    public void Clear()
+    {
+        Result.text = "";
+        CurrentTask.text = "";
+    }
     public void Addition()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        
-        {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = (_value1 + _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + "+";
+        Result.text="";
+        operationSelector = 1;
     }
     public void Substraction()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = (_value1 - _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + "-";
+        Result.text = "";
+        operationSelector = 2;
     }
     public void Multiplication()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = (_value1 * _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + "*";
+        Result.text = "";
+        operationSelector = 3;
     }
     public void Division()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        {
-            Result.text = "Empty field";
-        }
-        else if (_value2 == 0)
-        {
-            Result.text = "Can't divide by 0";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = (_value1 / _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + "/";
+        Result.text = "";
+        operationSelector = 4;
     }
     public void FindMin()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = Mathf.Min(_value1, _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " min?";
+        Result.text = "";
+        operationSelector = 5;
     }
     public void FindMax()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
-        {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = Mathf.Max(_value1, _value2).ToString("");
-        }
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " max?";
+        Result.text = "";
+        operationSelector = 6;
     }
     public void Power()
     {
-        if (string.IsNullOrEmpty(textField1.text) || string.IsNullOrEmpty(textField2.text))
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " pow";
+        Result.text = "";
+        operationSelector = 7;
+    }
+    public void Logarithm10()
+    {
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " log10";
+        Result.text = Mathf.Log10(_value1).ToString("");
+    }
+    public void Logarithm2()
+    {
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " log2";
+        Result.text = Mathf.Log(_value1).ToString("");
+    }
+    public void PiNumber()
+    {
+        Result.text = Mathf.PI.ToString("");
+    }
+    public void Sinus()
+    {
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " sinus";
+        Result.text = Mathf.Sin(_value1).ToString("");
+    }
+    public void Cotangent()
+    {
+        _value1 = float.Parse(Result.text);
+        CurrentTask.text = _value1.ToString() + " cotangent";
+        Result.text = (1/Mathf.Tan(_value1)).ToString("");
+    }
+    public void Equals()
+    {
+        switch (operationSelector)
         {
-            Result.text = "Empty field";
-        }
-        else
-        {
-            _value1 = float.Parse(textField1.text);
-            _value2 = float.Parse(textField2.text);
-            Result.text = Mathf.Pow(_value1, _value2).ToString("");
+            case 1:
+                _value2 = _value1 + float.Parse(Result.text);
+                Result.text = _value2.ToString("");
+                CurrentTask.text = "";
+                break;
+            case 2:
+                _value2 = _value1 - float.Parse(Result.text);
+                Result.text = _value2.ToString("");
+                CurrentTask.text = "";
+                break;
+            case 3:
+                _value2 = _value1 * float.Parse(Result.text);
+                Result.text = _value2.ToString("");
+                CurrentTask.text = "";
+                break;
+            case 4:
+                if (Result.text == "0")
+                {
+                    Result.text = "Can't divide by 0";
+                }
+                else
+                {
+                    _value2 = _value1 / float.Parse(Result.text);
+                    Result.text = _value2.ToString("");
+                    CurrentTask.text = "";
+                }
+                break;
+            case 5:
+                _value2 = float.Parse(Result.text);
+                Result.text = "Min is "+ Mathf.Min(_value1, _value2).ToString("");
+                CurrentTask.text = "";
+                break;
+            case 6:
+                _value2 = float.Parse(Result.text);
+                Result.text = "Max is " + Mathf.Max(_value1, _value2).ToString("");
+                CurrentTask.text = "";
+                break;
+            case 7:
+                _value2 = float.Parse(Result.text);
+                Result.text = Mathf.Pow(_value1, _value2).ToString("");
+                CurrentTask.text = "";
+                break;
         }
     }
 }
---
+++ My project/Assets/Scripts/NumericButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumericButton : MonoBehaviour
+{
+    public enum NumericType
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Comma = ','
+    }
+    public NumericType Numeric;
+    public Calculator Calculator;
+    public void Click()
+    {
+        Calculator.AddNumeric((int)Numeric);
+    }
+}
